67e13b4ee66ec709bb89da55f21caf88365caa29
DEVPRIMIS <primisdev4@gmail.com>
2025-08-04 17:29:51 -0400

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,13 @@
+const settings = {
+  packname: 'ɴᴇxɪ ᴍɪɴɪ ʙᴏᴛ',
+  author: 'Ds Primis',
+  botName: "ɴᴇxɪ ᴍɪɴɪ ʙᴏᴛ",
+  botOwner: 'Nexi', // Your name
+  ownerNumber: '50940938093', //Set your number here without + symbol.
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "1.0.0",
+};
+
+module.exports = settings;
